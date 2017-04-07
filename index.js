ab48788260ef4c744de7c9d79114a83c5fd497b6
Daniel Cardoso <hello@danielcardoso.net>
2017-04-08 00:15:08 +0100

    Add dependencies

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+// Native
+const path = require('path');
+
+// Packages
+const nodeVersion = require('node-version');
+
 // Throw an error if node version is too low
 if (nodeVersion.major < 6) {
   console.error('Error! HTML Pages requires at least version 6 of Node. Please upgrade!');
