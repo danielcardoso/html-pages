a7eae8203a7d87e8cf1c7bd3c261a1f9371681d0
Daniel Cardoso <hello@danielcardoso.net>
2017-04-08 00:03:35 +0100

    Minor updates

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# html-pages
+HTML Pages
+==========
+
+Simple development http server for file serving and directory listing.
+
+
+## Usage
+
+Install it (needs at least Node LTS):
+
+```bash
+npm install -g html-pages
+```
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
 
+// Throw an error if node version is too low
+if (nodeVersion.major < 6) {
+  console.error('Error! HTML Pages requires at least version 6 of Node. Please upgrade!');
+  process.exit(1);
+}
 
 console.log('HTML Pages will be available soon...')
